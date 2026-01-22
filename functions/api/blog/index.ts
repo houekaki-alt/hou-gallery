e1285c8bd150a301fcabe8c6ab45f107ac0c1fe5
houekaki-alt <hou.ekaki@gmail.com>
2026-01-22 22:34:10 +0900

    Update index.ts

---
+++ functions/api/blog/index.ts
@@ -0,0 +1,9 @@
+export async function onRequestGet({ env }) {
+  const res = await env.BLOG_API.fetch("https://dummy/");
+  return new Response(await res.text(), {
+    status: res.status,
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  });
+}
