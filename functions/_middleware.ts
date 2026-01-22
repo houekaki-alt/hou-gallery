d81ad0d0165a36515849a74ac05409b597ec1500
houekaki-alt <hou.ekaki@gmail.com>
2026-01-22 14:03:34 +0900

    Update _middleware.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -2,7 +2,7 @@ export const onRequest: PagesFunction = async (context) => {
   const url = new URL(context.request.url);
 
  
-  if (url.pathname === "/record-2026.html") {
+  if (url.pathname === "/inside.html") {
     const cookie = context.request.headers.get("Cookie") || "";
     const ok = cookie.includes("hou_secret=ok");
     if (!ok) {
