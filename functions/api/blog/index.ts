1880f04569a96aab7d8ca45a0f1fd0d76bef8d6a
houekaki-alt <hou.ekaki@gmail.com>
2026-01-23 19:41:25 +0900

    Update index.ts

--- functions/api/blog/index.ts
+++ functions/api/blog/index.ts
@@ -1,10 +1,9 @@
-export async function onRequestGet({ env }) {
+export async function onRequestGet({ env }: any) {
   const res = await env.BLOG_API.fetch("https://dummy/api/blog");
   return new Response(await res.text(), {
     status: res.status,
     headers: {
       "Content-Type": "application/json; charset=utf-8",
-      "Access-Control-Allow-Origin": "*",
     },
   });
 }
