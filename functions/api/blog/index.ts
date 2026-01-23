052856be3e41508e3fc7fc93085e56249961bd79
houekaki-alt <hou.ekaki@gmail.com>
2026-01-23 21:14:41 +0900

    Delete functions/api/blog directory

--- functions/api/blog/index.ts
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet({ env }: any) {
-  const res = await env.BLOG_API.fetch("https://dummy/api/blog");
-  return new Response(await res.text(), {
-    status: res.status,
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-  });
-}
